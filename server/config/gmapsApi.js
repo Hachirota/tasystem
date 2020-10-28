83673586e47842eb725069f6d6d4f91ba4318a61
Carl <carloftheboyles@gmail.com>
2020-10-28 15:05:48 +0000

    client for google maps api

---
+++ server/config/gmapsApi.js
@@ -0,0 +1,6 @@
+const { Client } = require("@googlemaps/google-maps-services-js");
+
+// Instantiate Google Maps API Client
+const gmapsApiClient = new Client({});
+
+module.exports = gmapsApiClient;
