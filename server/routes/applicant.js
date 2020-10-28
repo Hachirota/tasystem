b81b3699a74cb33d45d50434dc84009b6f559134
Carl <carloftheboyles@gmail.com>
2020-10-28 15:06:32 +0000

    post route created w gmaps api integration

--- server/routes/applicant.js
+++ server/routes/applicant.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const Applicant = require("../models/Applicant");
+const gmaps = require("../config/gmapsApi");
 
 // @desc Get All Applicants
 // @route GET /applicant
@@ -20,8 +21,29 @@ router.get("/", async (req, res) => {
 // @route POST /applicant
 router.post("/", async (req, res) => {
   try {
-    console.log(req.body);
+    let appBody = req.body;
+
+    let address =
+      appBody.address1 +
+      " " +
+      appBody.address2 +
+      " " +
+      appBody.eircode +
+      " " +
+      appBody.county +
+      " " +
+      appBody.country;
+
+    let encAddress = address.replace(/ /g, "%20");
+
+    let id = await gmaps.geocode({
+      params: { address: encAddress, key: process.env.GOOGLE_MAPS_API_KEY },
+    });
+
+    appBody.place_id = id.data.results[0].place_id;
+
     await Applicant.create(req.body);
+
     res.status(200).send();
   } catch (error) {
     console.error(error);
